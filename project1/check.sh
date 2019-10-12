7f468eee289a5c6378b6c5c5fc13dd7a0f512a6d
Wei,Geng <vigeng@qq.com>
2019-10-12 23:03:39 +0800

    pj1

--- project1/check.sh
+++ project1/check.sh
@@ -16,7 +16,7 @@ do
   echo ""
   id=${file:0:11}
   newName="$id.py"
-  mv $file $newName
+#  mv $file $newName
   python $newName
 	read -p "Please judge: " input_char
 	if [ $input_char = $F ]
@@ -27,3 +27,5 @@ do
 		echo $info >> ../rst.txt
 	fi
 done
+
+echo ojbk
